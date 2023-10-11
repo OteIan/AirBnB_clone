5e9bafd18f7d5601e9a4e1f8237d07e50e201479
MarkMusili <musiliyrn@gmail.com>
2023-10-11 18:14:59 +0300

    Base model upgrade

--- models/basemodel.py
+++ models/basemodel.py
@@ -9,20 +9,34 @@ for the other classes in the application
 class BaseModel:
     """
     Base model that will act as the base class
-    Attributes:
+    Takes in kwargs specified by the user
+    Attributes default values:
         id (str): Unique ID associated with an instance
         created_at (datetime): time the instance was created
         updated_at (datetime): time the instance was updated
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Class constructor
 
         Initializes the id, created_at, and updated_at attributes.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        format = '%Y-%m-%dT%H:%M:%S.%f'
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.strptime(value, format))
+                    else:
+                        setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -31,7 +45,7 @@ class BaseModel:
             str: String representation of the instance
         """
         class_name = self.__class__.__name__
-        return f"[{class_name}] ({self.id}) {self.__dict__}"
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def save(self):
         """
@@ -45,17 +59,8 @@ class BaseModel:
         Return:
             dict: dictionary respresentaion
         """
-        class_name = self.__class__.__name__
-        created_f = self.created_at.isoformat()
-        updated_f = self.updated_at.isoformat()
-
-        obj_dict = {
-            "__class__": class_name,
-            "id": self.id,
-            "created_at": created_f,
-            "updated_at": updated_f
-        }
-
-        obj_dict.update(self.__dict__)
-
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
