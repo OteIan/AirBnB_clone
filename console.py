671066a812b3ab3791735209bc106ec5dabf1ad6
Ote Ian <oteian9@gmail.com>
2023-10-14 12:03:53 +0300

    Addes shebang

--- console.py
+++ console.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import cmd
 import re
 from models.base_model import BaseModel
--- models/amenity.py
+++ models/amenity.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from models.base_model import BaseModel
 """
 This module defines a class that stores name of an amenity
